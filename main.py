8e3e6046ddb3173b1f0f1ba53c9aa05aaeab3f10
Satyajit2024 <satyajit.bariflo@outlook.com>
2024-05-31 15:58:34 +0530

    mqtt

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from mqtt_update import MqttConnect
+from mqtt_script import MqttConnect
 import time
 from datetime import datetime
 from random import uniform
-import json
 from paho.mqtt.client import Client
 import threading
 
-
 mq = MqttConnect()
-mq.topic = ["578689832956829","542484815423712","372582595849208"]
+mq.topic = ["299104867328041"]
+
+stop_event = threading.Event()
 
 def post_data_to_publish():
     mq.connect_to_broker()
-    while True:
+    while not stop_event.is_set():
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         for i in mq.topic:
-            rand_num = uniform(2.5,5.2)
-            rand_num2 = uniform(210.0 , 220.0)
+            rand_num = uniform(2.5, 5.2)
+            rand_num2 = uniform(210.0, 220.0)
             mq.data_publish({"dataPoint": now, "paramType": 'current', "paramValue": rand_num, "deviceId": i})
             mq.data_publish({"dataPoint": now, "paramType": 'voltage', "paramValue": rand_num2, "deviceId": i})
         time.sleep(5)
-
-
 
 def data_subscribe():
     mqtt_client = Client()
@@ -31,12 +29,26 @@ def data_subscribe():
     mqtt_client.connect(mq._mqttBroker, port=mq._port)
     mqtt_client.loop_start()
     try:
-        while True:
-            pass
+        while not stop_event.is_set():
+            time.sleep(1)
     except KeyboardInterrupt:
-        mqtt_client.loop_start()
-
+        pass
+    finally:
+        mqtt_client.loop_stop()
 
 if __name__ == '__main__':
-    threading.Thread(target=post_data_to_publish).start()
-    threading.Thread(target=data_subscribe).start()
+    try:
+        pub_thread = threading.Thread(target=post_data_to_publish)
+        sub_thread = threading.Thread(target=data_subscribe)
+        
+        pub_thread.start()
+        sub_thread.start()
+
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping threads...")
+        stop_event.set()
+        pub_thread.join()
+        sub_thread.join()
+        print("Threads stopped.")
